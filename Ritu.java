cac1463f6c93fe39aa738aecde7674d131f7c3e9
Ritutinker <110534729+Ritutinker@users.noreply.github.com>
2023-06-25 10:37:14 +0530

    Add files via upload

---
+++ Ritu.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+        // Hello World!
+    }
+}
+C:\Users\ASUS>cd desktop
+
+C:\Users\ASUS\Desktop>javac Ritu.java
+
+C:\Users\ASUS\Desktop>java Ritu.java
+Hello World!
